47dba7bcafa9c14ee4cc1d80c87e03843659dfe0
Igor Rogachov <igor@MacBook-Air-Igor.local>
2019-01-07 16:54:13 +0200

    Added django tamplates in index.html

--- spaceGrass/shop/views.py
+++ spaceGrass/shop/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from shop.models import Category, Product
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'index.html', {})
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories
+    }
+
+    return render(request, 'index.html', context)
