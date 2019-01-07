21fb80d799b9b8fc01b439c515f5f668e1b563e1
Igor Rogachov <igor@MacBook-Air-Igor.local>
2019-01-07 17:35:15 +0200

    Added items in database

--- spaceGrass/shop/models.py
+++ spaceGrass/shop/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from transliterate import translit
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 # Create your models here.
+
+def image_folder(instance, filename):
+    filename = instance.slug + '.' + filename.split('.')[1]
+    return "{0}/{1}".format(instance.category.slug, filename)
+
+def pre_save_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(translit(instance.name, reversed=True))
+
+class ProductManager(models.Manager):
+    def all(self, *args, **kwargs):
+        return super(ProductManager, self).get_queryset().filter(available=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
@@ -13,7 +29,7 @@ class Grass(models.Model):
     name = models.CharField(max_length=64)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     slug = models.SlugField(blank=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to=image_folder)
     description = models.TextField()
 
     def __str__(self):
@@ -25,6 +41,8 @@ class Product(models.Model):
     grass = models.ForeignKey(Grass, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
     count = models.PositiveIntegerField(default=1)
+    available = models.BooleanField(default=True)
+    objects = ProductManager()
 
     def __str__(self):
         return self.name
@@ -45,9 +63,11 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
-    slug = models.SlugField(blank=True)
     email = models.EmailField()
 
     def __str__(self):
         return str(self.id)
 
+pre_save.connect(pre_save_slug, sender=Category)
+pre_save.connect(pre_save_slug, sender=Product)
+pre_save.connect(pre_save_slug, sender=Grass)
