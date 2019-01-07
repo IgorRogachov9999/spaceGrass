15ace11ceb2bf46f086a33607595b28e27382341
Igor Rogachov <igor@MacBook-Air-Igor.local>
2019-01-07 13:34:08 +0200

    Created models and installed PostgresSQL 11

---
+++ spaceGrass/shop/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Grass(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True)
+    image = models.ImageField()
+    title = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(blank=True)
+    grass = models.ForeignKey(Grass, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+    count = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Delivery(models.Model):
+    location = models.CharField(max_length=256)
+    
+    def __str__(self):
+        return self.name
+ 
+class Payment(models.Model):
+    name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return str(self.id)
+
--- spaceGrass/spaceGrass/settings.py
+++ spaceGrass/spaceGrass/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'spaceGrass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER' : 'user_name',
+        'PASSWORD' : 'password',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
